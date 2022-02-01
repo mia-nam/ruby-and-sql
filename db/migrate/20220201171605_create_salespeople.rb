1687d31b9da52625e4dcf2aa844e35446031184c
mia-nam <mia.nam@kellogg.northwestern.edu>
2022-02-01 17:18:07 +0000

    save02

--- db/migrate/20220201171605_create_salespeople.rb
+++ db/migrate/20220201171605_create_salespeople.rb
@@ -1,7 +1,9 @@
 class CreateSalespeople < ActiveRecord::Migration[7.0]
   def change
     create_table :salespeople do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :email 
       t.timestamps
     end
   end
