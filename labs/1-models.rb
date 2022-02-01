fed6cf25925b2c8a31af402f9c8e3fc51eb2d657
mia-nam <mia.nam@kellogg.northwestern.edu>
2022-02-01 17:16:53 +0000

    saving

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -13,6 +13,55 @@ Contact.destroy_all
 
 # 2. create 1-2 new contacts for each company (they can be made up)
 
+# Andy Jassy - andy@amazon.com
+# Craig Federighi - craig@apple.com
+# Elon Musk - elon@tesla.com
+# Tim Cook - tim@apple.com
+
+apple = Company.where({ name:"Apple, Inc."})[0]
+puts apple.id
+apple_id = apple.id
+values = { first_name: "Craig",
+            last_name: "Federight",
+            email:"craig@apple.com",
+            phone_number: "888-555-1212",
+            company_id: apple_id
+        }
+contact = Contact.new(values) #insert new valeus to contact
+contact.save
+
+amazon = Company.where({ name:"Aamazon.com, Inc."})[0]
+puts amazon.id
+amazon_id = amazon.id
+
+contact = Contact.new
+contact.first_name = "Andy"
+contact.last_name = "Jassy"
+contact.email = "andy@amazon.com"
+contact.phone_number = "888-555-1313"
+contact.company_id = amazon_id
+contact.save
+
+contact = Contact.new
+contact.first_name = "Tim"
+contact.last_name = "Cook"
+contact.email = "tim@apple.com"
+contact.phone_number = "888-555-1111"
+contact.company_id = apple_id
+contact.save
+
+tesla = Company.where({ name:"Tesla, Inc."})[0]
+puts tesla.id
+tesla_id = tesla.id
+
+contact = Contact.new
+contact.first_name = "Elon"
+contact.last_name = "Musk"
+contact.email = "elon@tesla.com"
+contact.phone_number = "888-999-9999"
+contact.company_id = tesla_id
+contact.save
+
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
 # ---------------------------------
@@ -21,3 +70,12 @@ Contact.destroy_all
 # Craig Federighi - craig@apple.com
 # Elon Musk - elon@tesla.com
 # Tim Cook - tim@apple.com
+
+puts "There are #{Contact.all.count} contacts"
+all_contacts = Contact.all
+
+
+for x in all_contacts
+    puts "#{x.first_name} #{x.last_name} - #{x.email}"
+
+end
--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -5,6 +5,12 @@
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
 
+puts "There are #{Contact.all.count} contacts"
+all_contacts = Contact.all
+for x in all_contacts
+    puts "#{x.first_name} #{x.last_name} - #{x.email} - #{x.company.name}"
+end
+
 # ---------------------------------
 # Contacts: 4
 # Andy Jassy - andy@amazon.com - Amazon.com, Inc.
@@ -13,6 +19,17 @@
 # Tim Cook - tim@apple.com - Apple Inc.
 
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
+
+
+puts "---"
+for company in Company.all
+    puts company.name
+    for contact in company.contacts
+        puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+    end
+    puts " "
+end
+
 
 # ---------------------------------
 # Apple Inc.
